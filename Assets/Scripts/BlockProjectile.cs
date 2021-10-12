f8d40c1681a32ac26870fc358c3e5491cbc3af86
Damiano <damian.vlaicu@gmail.com>
2021-10-12 23:49:27 +0200

    first commit

---
+++ Assets/Scripts/BlockProjectile.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockProjectile : MonoBehaviour
+{
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) return;
+        Instantiate(this.cube, getSpawnPosition(other), other.transform.rotation);
+        Destroy(gameObject);
+    }
+
+    Vector3 getSpawnPosition(Collider other)
+    {
+        var dir = (other.transform.position - transform.position ).normalized;
+        Vector3 offset;
+
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y) && Mathf.Abs(dir.x) > Mathf.Abs(dir.z))
+            offset = new Vector3(dir.x / Mathf.Abs(dir.x), 0, 0);
+        else if (Mathf.Abs(dir.y) > Mathf.Abs(dir.z)) offset = new Vector3(0, dir.y / Mathf.Abs(dir.y), 0);
+        else offset = new Vector3(0, 0, dir.z / Mathf.Abs(dir.z));
+
+        return other.transform.position - offset;
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float angle = 45f;
+    public float shootVelocity = 10f;
+    public GameObject projectile;
+
+    private CinemachineFreeLook cineCam; //TODO somewhere else
+
+    private Transform t;
+    private Camera cam;
+    private float timeStep = 0.05f;
+    private LineRenderer lr;
+    private bool cameraFrozen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        cam = Camera.main;
+        lr = GetComponent<LineRenderer>();
+        cineCam = GameObject.Find("Third Person Camera").GetComponent<CinemachineFreeLook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (cameraFrozen) freezeCamera();
+            cameraFrozen = true;
+            var pos = t.position;
+            Plane plane = new Plane(Vector3.up, 0);
+            Vector3 worldPosition = new Vector3();
+
+            angle = Mathf.Clamp(angle - Input.mouseScrollDelta.y, 0, 85);
+
+            float distance;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance)) worldPosition = ray.GetPoint(distance);
+
+            var forward = worldPosition - pos;
+            forward.y = 0;
+
+            t.rotation = Quaternion.LookRotation(forward, Vector3.up);
+
+            var trajectoryPoints = calculateTrajectory();
+            lr.positionCount = trajectoryPoints.Count;
+            lr.SetPositions(trajectoryPoints.ToArray());
+            
+            if (Input.GetKeyDown(KeyCode.Mouse0)) shoot();
+        }
+        else
+        {
+            lr.positionCount = 0;
+            if (cameraFrozen) unFreezeCamera();
+            cameraFrozen = false;
+        }
+    }
+
+    List<Vector3> calculateTrajectory()
+    {
+        var startPos = t.position;
+        var velocity = calculateVelocity();
+        var gravity = new Vector3(0, -9.81f, 0);
+
+        var points = new List<Vector3>();
+        var time = 0f;
+
+        var currTrajectoryPoint = startPos;
+
+        while (currTrajectoryPoint.y > 0)
+        {
+            time += timeStep;
+            currTrajectoryPoint = startPos + velocity * time + 0.5f * gravity * Mathf.Pow(time, 2);
+            points.Add(currTrajectoryPoint);
+        }
+
+        return points;
+    }
+
+    void shoot()
+    {
+        var velocity = calculateVelocity();
+        var obj = Instantiate(projectile, t.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().velocity = velocity;
+    }
+
+    Vector3 calculateVelocity()
+    {
+        var dir = transform.forward;
+        return new Vector3(dir.x * shootVelocity * Mathf.Cos(Mathf.Deg2Rad * angle),
+            Mathf.Sin(Mathf.Deg2Rad * angle) * shootVelocity, dir.z * shootVelocity * Mathf.Cos(Mathf.Deg2Rad * angle));
+    }
+
+    //TODO place somewhere else
+    void freezeCamera()
+    {
+        cineCam.m_XAxis.m_InputAxisName = "";
+        cineCam.m_YAxis.m_InputAxisName = "";
+    }
+
+    void unFreezeCamera()
+    {
+        cineCam.m_XAxis.m_InputAxisName = "Mouse X";
+        cineCam.m_YAxis.m_InputAxisName = "Mouse Y";
+    }
+}
